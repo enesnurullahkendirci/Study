aabfbbce7802517dd24001b2a0d8aa802942bfe3
Enes Kendirci <enesnkendirci@gmail.com>
2024-05-18 01:19:46 +0300

    create customSearchBar

--- EnesKendirciCaseStudy/ProductList/ProductListViewController.swift
+++ EnesKendirciCaseStudy/ProductList/ProductListViewController.swift
@@ -8,8 +8,32 @@
 import UIKit
 
 final class ProductListViewController: BaseViewController {
+    private lazy var customSearchBar: CustomSearchBar = {
+        let searchBar = CustomSearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupSearchBar()
     }
+    
+    private func setupSearchBar() {
+        view.addSubview(customSearchBar)
+        NSLayoutConstraint.activate([
+            customSearchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 14),
+            customSearchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            customSearchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            customSearchBar.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        customSearchBar.setDelegate(self)
+    }
+}
+
+extension ProductListViewController: UISearchBarDelegate {
+
 }
---
+++ EnesKendirciCaseStudy/UIComponents/CustomSearchBar.swift
@@ -0,0 +1,44 @@
+//
+//  CustomSearchBar.swift
+//  EnesKendirciCaseStudy
+//
+//  Created by Enes KENDİRCİ on 18.05.2024.
+//
+
+import UIKit
+
+final class CustomSearchBar: UIView {
+
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "Search"
+        searchBar.searchBarStyle = .minimal
+        searchBar.setImage(UIImage(systemName: "magnifyingglass"), for: .search, state: .normal)
+        return searchBar
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    private func setupView() {
+        addSubview(searchBar)
+        NSLayoutConstraint.activate([
+            searchBar.leadingAnchor.constraint(equalTo: leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: trailingAnchor),
+            searchBar.topAnchor.constraint(equalTo: topAnchor),
+            searchBar.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    func setDelegate(_ delegate: UISearchBarDelegate) {
+        searchBar.delegate = delegate
+    }
+}
