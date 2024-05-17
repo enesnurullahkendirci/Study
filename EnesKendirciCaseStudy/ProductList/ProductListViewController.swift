a373ec68e58885cb14e4b65404245dd731030b10
Enes Kendirci <enesnkendirci@gmail.com>
2024-05-17 19:25:18 +0300

    delete main.storyboard

---
+++ EnesKendirciCaseStudy/ProductList/ProductListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProductListViewController.swift
+//  EnesKendirciCaseStudy
+//
+//  Created by Enes KENDİRCİ on 17.05.2024.
+//
+
+import UIKit
+
+final class ProductListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        title = "Product List"
+        
+    }
+}
