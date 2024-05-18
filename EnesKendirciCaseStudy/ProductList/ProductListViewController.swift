e03d05f3f0137b66981d2367aa7736e00364a219
Enes Kendirci <enesnkendirci@gmail.com>
2024-05-18 21:02:56 +0300

    add filter views

--- EnesKendirciCaseStudy/ProductList/ProductListViewController.swift
+++ EnesKendirciCaseStudy/ProductList/ProductListViewController.swift
@@ -14,11 +14,33 @@ final class ProductListViewController: BaseViewController {
         return searchBar
     }()
     
+    private lazy var filterView: UIView = {
+        let filterView = UIView()
+        filterView.translatesAutoresizingMaskIntoConstraints = false
+        return filterView
+    }()
+    
+    private lazy var filterLabel: UILabel = {
+        let filterLabel = UILabel()
+        filterLabel.translatesAutoresizingMaskIntoConstraints = false
+        filterLabel.text = "Filters:"
+        return filterLabel
+    }()
+    
+    private lazy var filterButton: UIButton = {
+        let filterButton = UIButton()
+        filterButton.translatesAutoresizingMaskIntoConstraints = false
+        filterButton.setTitle("Select Filter", for: .normal)
+        filterButton.setTitleColor(.black, for: .normal)
+        filterButton.backgroundColor = UIColor(hex: "#D9D9D9")
+        return filterButton
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupSearchBar()
+        setupFilterView()
     }
     
     private func setupSearchBar() {
@@ -31,6 +53,27 @@ final class ProductListViewController: BaseViewController {
         ])
         
         customSearchBar.setDelegate(self)
+    }
+    
+    private func setupFilterView() {
+        view.addSubview(filterView)
+        filterView.addSubview(filterLabel)
+        filterView.addSubview(filterButton)
+        
+        NSLayoutConstraint.activate([
+            filterView.topAnchor.constraint(equalTo: customSearchBar.bottomAnchor, constant: 14),
+            filterView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            filterView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            filterView.heightAnchor.constraint(equalToConstant: 40),
+            
+            filterLabel.leadingAnchor.constraint(equalTo: filterView.leadingAnchor),
+            filterLabel.centerYAnchor.constraint(equalTo: filterView.centerYAnchor),
+            
+            filterButton.trailingAnchor.constraint(equalTo: filterView.trailingAnchor),
+            filterButton.topAnchor.constraint(equalTo: filterView.topAnchor),
+            filterButton.bottomAnchor.constraint(equalTo: filterView.bottomAnchor),
+            filterButton.widthAnchor.constraint(equalToConstant: 158),
+        ])
     }
 }
 
